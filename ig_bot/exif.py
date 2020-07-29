7dedee42c43282c33e4ec37dfa17677f32cbaf26
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-07-28 20:56:48 -0500

    Create exif.py
    extraer los datos exif de una foto jpeg

---
+++ ig_bot/exif.py
@@ -0,0 +1,24 @@
+"""
+extraer los datos exif de una foto jpeg
+"""
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+# path to the image or video
+imagename = "/Users/dsalzedo/Documents/Python/1.jpg"
+
+# read the image data using PIL
+image = Image.open(imagename)
+
+# extract EXIF data
+exifdata = image.getexif()
+
+# iterating over all EXIF data fields
+for tag_id in exifdata:
+    # get the tag name, instead of human unreadable tag id
+    tag = TAGS.get(tag_id, tag_id)
+    data = exifdata.get(tag_id)
+    # decode bytes
+    if isinstance(data, bytes):
+        data = data.decode()
+    print(f"{tag:25}: {data}")
