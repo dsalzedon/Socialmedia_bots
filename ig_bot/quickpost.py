69a425cd5acac997ba4e5e76c4b887867e2041eb
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-07-30 23:04:52 -0500

    Update quickpost.py

--- ig_bot/quickpost.py
+++ ig_bot/quickpost.py
@@ -1,12 +1,15 @@
 """
 ESTE PROGRAMA ES SOLO PARA SUBIR RAPIDAMENTE UNA FOTO
 UNO DANDO EL DIA Y EL NUMERO DE LA FOTO
-
 pip install -U instabot
 git clone https://github.com/instagrambot/instabot --recursive
 cd instabot/examples
 
 el user id de notivan es 24994235608
+
+x va desde 0.1 a 1, izquierda a derecha
+y desde 0.1 a 1, arriba a abajo
+
 """
 
 from instabot import Bot
@@ -27,6 +30,6 @@ img = "/Users/dsalzedo/Documents/Python/instabot/pic/1.jpg"
 
 user_id_1 = 24994235608
 
-tags = [{'user_id': user_id_1, 'x': 0.1, 'y': 0.9}]
+tags = [{'notivan': user_id_1, 'x': 0.1, 'y': 0.9}]
 
 bot.upload_photo(img, user_tags=tags, caption=cap)
