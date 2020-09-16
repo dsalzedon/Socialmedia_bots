71dbe3d1c88f39c7cf85bae4a6d84210293a191a
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-09-16 14:49:11 -0500

    Update white_border.py

--- ig_bot/white_border.py
+++ ig_bot/white_border.py
@@ -9,6 +9,25 @@ from PIL import Image, ImageOps
 from random import choice
 
 
+def check_size(image_path):
+    img = Image.open(image_path)
+    img_size = img.size
+    if img_size[0] > img_size[1]:
+        border_select = choice(border_lst_hor).split("x")
+        add_border(
+            image_path,
+            image_path,
+            border=(int(border_select[0]), int(border_select[1])),
+        )
+    else:
+        border_select = choice(border_lst_ver).split("x")
+        add_border(
+            image_path,
+            image_path,
+            border=(int(border_select[0]), int(border_select[1])),
+        )
+
+
 def add_border(input_image, output_image, border):
     img = Image.open(input_image)
 
@@ -22,14 +41,11 @@ def add_border(input_image, output_image, border):
 
 os.chdir("/Users/dsalzedo/Documents/Python/scripts/Random_Code/pic")
 
-border_lst = ["20x60", "45x60", "40x80"]
+border_lst_hor = ["30x70", "20x80", "40x80"]
+border_lst_ver = ["20x60", "45x60", "10x30"]
 
 for f in os.listdir():
     if f == ".DS_Store":
         pass
     else:
-        border_select = choice(border_lst).split("x")
-        print(border_select)
-        # print(str(border_select[0]), str(border_select[1]))
-        # print(f)
-        add_border(f, f, border=(int(border_select[0]), int(border_select[1])))
+        check_size(f)
