0ad0cd9fb037eabdb7bfc68a384e9ce313ca7e4a
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-09-16 14:48:50 -0500

    Update whiteborder_c1.py
    add condition for vertical or horizontal photos

--- ig_bot/whiteborder_c1.py
+++ ig_bot/whiteborder_c1.py
@@ -12,6 +12,25 @@ from PIL import Image, ImageOps
 from random import choice
 
 
+def check_size(image_path):
+    img = Image.open(image_path)
+    img_size = img.size
+    if img_size[0] > img_size[1]:
+        border_select = choice(border_hor).split("x")
+        add_border(
+            image_path,
+            image_path,
+            border=(int(border_select[0]), int(border_select[1])),
+        )
+    else:
+        border_select = choice(border_ver).split("x")
+        add_border(
+            image_path,
+            image_path,
+            border=(int(border_select[0]), int(border_select[1])),
+        )
+
+
 def add_border(input_image, output_image, border):
     img = Image.open(input_image)
 
@@ -26,12 +45,15 @@ def add_border(input_image, output_image, border):
 parser = argparse.ArgumentParser()
 parser.add_argument("path", type=str)
 args = parser.parse_args()
-f = args.path
+img_file = args.path
 
-# en border_lst cambias los valores para los marcos y puedes añadir más
-border_lst = ["20x60", "45x60", "40x80"]
+# En border_x cambias los valores para los marcos y puedes añadir más
+# Para añadir otro marco, ponle una coma(,) al ultimo y usa el formato VALORxVALOR entre comillas("")
+# e.g. ["4x4", 50x10, "50x100", "60x100"]
 
-# escoge un pseudo-aleatorio entre la lista de bordes
-border_select = choice(border_lst).split("x")
+border_hor = ["30x70", "20x80", "40x80"]
 
-add_border(f, f, border=(int(border_select[0]), int(border_select[1])))
+border_ver = ["20x60", "45x60", "10x30"]
+
+
+check_size(img_file)
